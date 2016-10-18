065d778438583f31fdba44d2a08d0b4726fcb747
Chris Atkins <chrisatkins55@gmail.com>
2016-10-17 23:28:02 -0400

    reverting

--- karma-headless.conf.js
+++ karma-headless.conf.js
@@ -1,8 +1,6 @@
 module.exports = function(config){
   var baseProjectConfig = require('./karma.conf.js');
 
-  process.env.PHANTOMJS_BIN = './node_modules/.bin/phantomjs';
-
   baseProjectConfig(config);
 
   config.set({
